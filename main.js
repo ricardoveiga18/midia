4d5a045899fe747ef9367cab30dac359bf27dbbd
Ricardo Manoel Koenig Veiga <99299941+ricardoveiga18@users.noreply.github.com>
2023-11-30 18:58:15 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+function tocaSom (idElementoAudio) {
+    const elemento = document.querySelector(idElementoAudio);
+    if (elemento && elemento,localName === 'audio'){
+        elemento.play();
+    }else{
+        console.log("Elemneto não encontrado ou seletor inválido");
+    }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+    
+    tecla.onkeydown = function(evento){
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa')
+        }
+        
+    }
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa')
+    }
+}
